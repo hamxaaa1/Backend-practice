c9f633befd91439d066e0d4a5410dc0342c76f1d
Hamza Ashfaq <hamzaashfaq4322@gmail.com>
2024-12-29 04:21:44 +0500

    v1

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -23,7 +23,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('https://backend-practice-eta.vercel.app//auth/login', {
+      const response = await fetch('https://backend-practice-eta.vercel.app/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
